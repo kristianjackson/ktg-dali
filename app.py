0323f4dfc0cf5021c2ea2c72799d7e69ed75f885
Kristian Jackson <kristian.jackson@gmail.com>
2023-07-27 17:29:12 +0000

    adding a list of questions

--- app.py
+++ app.py
@@ -26,6 +26,13 @@ Kearney has helped the Federal Government improve its financial operations’ ov
 We understand the Federal Government’s need for efficiency and transparency.
 """
 
+prompts = [
+    "Provide a 300 word summary for the document",
+    "List any financial information that is present in the document",
+    "Are there references to any Public Laws, other legislation, or regulations",
+    "Does this impact Kearney & Company",
+]
+
 async def main():
 
     async def storeDocEmbeds(file, filename):
@@ -110,21 +117,23 @@ async def main():
         # container for text box
         container = st.container()
 
-        with container:
-            with st.form(key='my_form', clear_on_submit=True):
-                user_input = st.text_input("Query:", placeholder="e.g: Summarize the paper in a few sentences", key='input')
-                submit_button = st.form_submit_button(label='Send')
+        for item in prompts:
+            with container:
+                with st.form(key='my_form', clear_on_submit=True):
+                    user_input = st.text_input("Query:", placeholder="e.g: Summarize the paper in a few sentences", key='input', value=item)
+                    #submit_button = st.form_submit_button(label='Send')
 
-            if submit_button and user_input:
-                output = await conversational_chat(user_input)
-                st.session_state['past'].append(user_input)
-                st.session_state['generated'].append(output)
+                # if submit_button and user_input:
+                if user_input:
+                    output = await conversational_chat(user_input)
+                    st.session_state['past'].append(user_input)
+                    st.session_state['generated'].append(output)
 
-        if st.session_state['generated']:
-            with response_container:
-                for i in range(len(st.session_state['generated'])):
-                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-                    message(st.session_state["generated"][i], key=str(i))
+            if st.session_state['generated']:
+                with response_container:
+                    for i in range(len(st.session_state['generated'])):
+                        message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                        message(st.session_state["generated"][i], key=str(i))
 
 
 if __name__ == "__main__":
