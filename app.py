8a9e28b8968dadbe4011d777c3a1c375ae4126b9
Kristian Jackson <kristian.jackson@gmail.com>
2023-07-27 17:05:10 +0000

    removed user emoji avatar

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ async def main():
         if st.session_state['generated']:
             with response_container:
                 for i in range(len(st.session_state['generated'])):
-                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="blue_monster")
+                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
                     message(st.session_state["generated"][i], key=str(i))
 
 
